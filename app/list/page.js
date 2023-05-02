5cf30be02a0246994b90f8ff88480eec937bf814
snowriceDD <yoons100457@gmail.com>
2023-05-02 18:17:13 +0900

    "중간 저장"

--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,16 @@
+
 export default function List() {
+    let product = [['tomatoes', 40], ['pasta', 20], ['coconut', 150]];
+
     return (
       <div>
         <h2>Products</h2>
-        <div className="food">
-          <h4>상품명 $40</h4>
-        </div>
-        <div className="food">
-          <h4>상품명 $40</h4>
-        </div>
+        {product.map((e, i)=> {
+          return (<div className="food" key={i}>
+            <img src={`/food${i}.png`} alt={`food${i} 식품 이미지`}/>
+            <h4>{e[0]} ${e[1]}</h4>
+          </div>)
+        })}
       </div>
     )
   }
