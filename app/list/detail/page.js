d3ebfb931ed7d6f26ea87b5de3dd87c360c1d708
snowriceDD <yoons100457@gmail.com>
2023-05-02 17:15:59 +0900

    "feat: make a navbar"

---
+++ app/list/detail/page.js
@@ -0,0 +1,13 @@
+export default function Detail() {
+    return (
+      <div>
+        <h2>상품 디테일</h2>
+        <div className="food">
+          <h4>상품명 $40</h4>
+        </div>
+        <div className="food">
+          <h4>상품명 $40</h4>
+        </div>
+      </div>
+    )
+  }
--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 
+
 export default function Home() {
   return (
     <div>
-      
+      <h3>안녕</h3>
     </div>
   )
 }
