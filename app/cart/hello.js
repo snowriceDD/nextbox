885521294fad09ef6801e9019aa5d9898357c111
snowriceDD <yoons100457@gmail.com>
2023-05-03 23:22:50 +0900

    "feat : hello 분리"

---
+++ app/cart/hello.js
@@ -0,0 +1,8 @@
+export const Hello = () => {
+
+    return (
+        <>
+            <h1>안녕하세요</h1>
+        </>
+    );
+}
--- app/cart/page.js
+++ app/cart/page.js
@@ -1,17 +1,25 @@
+import age from "./data";
+import { Hello } from "./hello";
+
+
 export default function Cart() {
     return (
       <div>
+        <Hello />
         <h4 className="title">Cart</h4>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
+        <CartItem />
+        <CartItem />
       </div>
     )
   } 
+
+  const CartItem = () => {
+
+    return (
+      <div className="cart-item">
+          <p>상품명</p>
+          <p>$40</p>
+          <p>1개</p>
+        </div>
+    );
+  }
