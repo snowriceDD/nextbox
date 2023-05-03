4153faed400843c208de23574dc7bb1e7216ac3d
snowriceDD <yoons100457@gmail.com>
2023-05-03 23:33:41 +0900

    "props"

--- app/cart/page.js
+++ app/cart/page.js
@@ -3,21 +3,30 @@ import { Hello } from "./hello";
 
 
 export default function Cart() {
+  let cart = ['tomatoes', 'pasta'];
+
     return (
       <div>
         <Hello />
         <h4 className="title">Cart</h4>
-        <CartItem />
-        <CartItem />
+        <CartItem cart={cart[0]}/>
+        <CartItem cart={cart[1]}/>
+        <Banner name = {'현대'} />
+        <Banner name = {'롯데'} />
+        <Banner name = {'하나'} />
       </div>
     )
   } 
 
-  const CartItem = () => {
+  const Banner = (props) => {
+    return (<h5>{props.name}카드 결제 행사중</h5>)
+  }
+
+  const CartItem = (props) => {
 
     return (
       <div className="cart-item">
-          <p>상품명</p>
+          <p>{props.cart}</p>
           <p>$40</p>
           <p>1개</p>
         </div>
